5e6fcfa0666db7892912abb83381fc21a90c38fd
robinson15 <robinsonanthony1505@gmail.com>
2023-09-16 12:51:28 +0300

    add the template for landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'nirovi-web';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    // Your code goes here
+    window.addEventListener('load', () => {
+      const showMenuElement = document.querySelector('#showMenu');
+      if (showMenuElement) {
+        showMenuElement.addEventListener('click', (event) => {
+          const mobileNavElement = document.querySelector('#mobileNav');
+          if (mobileNavElement) {
+            mobileNavElement.classList.remove('hidden');
+          }
+        });
+      }
+
+      const hideMenuElement = document.querySelector('#hideMenu');
+      if (hideMenuElement) {
+        hideMenuElement.addEventListener('click', (event) => {
+          const mobileNavElement = document.querySelector('#mobileNav');
+          if (mobileNavElement) {
+            mobileNavElement.classList.add('hidden');
+          }
+        });
+      }
+
+      document.querySelectorAll('[toggleElement]').forEach((toggle) => {
+        toggle.addEventListener('click', (event) => {
+          const answerElement = toggle.querySelector('[answer]');
+          const caretElement = toggle.querySelector('img');
+          if (answerElement && caretElement) {
+            if (answerElement.classList.contains('hidden')) {
+              answerElement.classList.remove('hidden');
+              caretElement.classList.add('rotate-90');
+            } else {
+              answerElement.classList.add('hidden');
+              caretElement.classList.remove('rotate-90');
+            }
+          }
+        });
+      });
+    });
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+ 
+    content: ["./src/**/*.{html,ts}"],
+
+  // darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: "#94c5cc",
+      secondary: "#b4d2e7",
+      black: "black",
+      white: "white",
+    },
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
+    },
+    extend: {
+      backgroundImage: {
+        underline1: "url('./assets/icons/Underline1.svg')",
+        underline2: "url('./assets/icons/Underline2.svg')",
+        underline3: "url('./assets/icons/Underline3.svg')",
+        underline4: "url('./assets/icons/Underline4.svg')",
+        highlight3: "url('./assets/icons/Highlight3.svg')",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
